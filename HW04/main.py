ecf4162cdaa6e1979c9883fd03e5e25bcded28b1
Anisimova Karina <51861302+Kira5005-code@users.noreply.github.com>
2020-10-07 12:09:39 +0300

    fix style

--- HW04/main.py
+++ HW04/main.py
@@ -1,4 +1,5 @@
 import sys
+import test
 
 
 def split_and_keep(s, sep):
@@ -10,7 +11,8 @@ def split_and_keep(s, sep):
 
 def lexer(s):
     for i in range(len(s)):
-        if (s[i] == ' ' and s[i - 1] == ' ') or (s[i] == '\n' and s[i - 1] == '\n') or (s[i] == '\t' and s[i - 1] == '\t'):
+        if (s[i] == ' ' and s[i - 1] == ' ') or (s[i] == '\n' and s[i - 1] == '\n') or (
+                s[i] == '\t' and s[i - 1] == '\t'):
             continue
         yield s[i]
     while True:
@@ -20,7 +22,8 @@ def lexer(s):
 def position_colon(s):
     pos = 0
     for i in range(len(s)):
-        if (s[i] == ' ' and s[i - 1] == ' ') or (s[i] == '\n' and s[i - 1] == '\n') or (s[i] == '\t' and s[i - 1] == '\t'):
+        if (s[i] == ' ' and s[i - 1] == ' ') or (s[i] == '\n' and s[i - 1] == '\n') or (
+                s[i] == '\t' and s[i - 1] == '\t'):
             pos += 1
             continue
         pos += 1
@@ -49,34 +52,30 @@ class parser:
         self.current_pos_col = next(self.pos_col)
         self.current_pos_line = next(self.pos_line)
 
+    def step(self):
+        self.current = next(self.lex)
+        self.current_pos_col = next(self.pos_col)
+        self.current_pos_line = next(self.pos_line)
+
     def accept(self, c):
         if self.current == c:
-            self.current = next(self.lex)
-            self.current_pos_col = next(self.pos_col)
-            self.current_pos_line = next(self.pos_line)
+            self.step()
             return True
         return False
 
     def skip(self):
         while self.current == '\n' or self.current == '\t' or self.current == ' ':
-            self.current = next(self.lex)
-            self.current_pos_col = next(self.pos_col)
-            self.current_pos_line = next(self.pos_line)
+            self.step()
 
     def expect(self, c):
         if self.current == c:
-            self.current = next(self.lex)
-            self.current_pos_col = next(self.pos_col)
-            self.current_pos_line = next(self.pos_line)
+            self.step()
             return True
-        # print("Unexpected character", self.current, "expected", c, self.current_pos)
         return False
 
     def letter(self):
         if self.current.isalpha():
-            self.current = next(self.lex)
-            self.current_pos_col = next(self.pos_col)
-            self.current_pos_line = next(self.pos_line)
+            self.step()
             return True
         else:
             return False
@@ -85,9 +84,7 @@ class parser:
         if not (self.current.isalpha() or self.current == '_'):
             return False
         while self.current.isalnum() or self.current == '_':
-            self.current = next(self.lex)
-            self.current_pos_col = next(self.pos_col)
-            self.current_pos_line = next(self.pos_line)
+            self.step()
         return True
 
     def tail(self):
@@ -150,122 +147,8 @@ class parser:
         return True
 
 
-class Tester:
-    def run(self):
-        if not self.good_test1():
-            print("Failed good_test1")
-        if not self.good_test2():
-            print("Failed good_test2")
-        if not self.good_test3():
-            print("Failed good_test3")
-        if not self.good_test4():
-            print("Failed good_test4")
-        if not self.good_test5():
-            print("Failed good_test5")
-        if not self.good_test6():
-            print("Failed good_test6")
-        if not self.good_test7():
-            print("Failed good_test7")
-        if not self.good_test8():
-            print("Failed good_test8")
-        if not self.good_test9():
-            print("Failed good_test9")
-        if not self.good_test10():
-            print("Failed good_test10")
-        if not self.good_test11():
-            print("Failed good_test11")
-        if not self.bad_test1():
-            print("Failed bad_test1")
-        if not self.bad_test2():
-            print("Failed bad_test2")
-        if not self.bad_test3():
-            print("Failed bad_test3")
-        if not self.bad_test4():
-            print("Failed bad_test4")
-        if not self.bad_test5():
-            print("Failed bad_test5")
-        if not self.bad_test6():
-            print("Failed bad_test6")
-        if not self.bad_test7():
-            print("Failed bad_test7")
-        if not self.bad_test8():
-            print("Failed bad_test8")
-        if not self.bad_test9():
-            print("Failed bad_test9")
-        if not self.bad_test10():
-            print("Failed bad_test10")
-        if not self.bad_test11():
-            print("Failed bad_test11")
-
-    def good_test1(self):
-        return parser('f.').process()[0] == -1
-
-    def good_test2(self):
-        return parser('f :- g.').process()[0] == -1
-
-    def good_test3(self):
-        return parser('f :- g, h; t.').process()[0] == -1
-
-    def good_test4(self):
-        return parser('f :- g, (h; t).').process()[0] == -1
-
-    def good_test5(self):
-        return parser('f :- (g),h.').process()[0] == -1
-
-    def good_test6(self):
-        return parser("f :- ((gh, kl); (qw, po); k) ;p.").process()[0] == -1
-
-    def good_test7(self):
-        return parser('f :- ((g),(h)).').process()[0] == -1
-
-    def good_test8(self):
-        return parser('f :- ((g);(h)).').process()[0] == -1
-
-    def good_test9(self):
-        return parser('fAb :- ((g),(h)).').process()[0] == -1
-
-    def good_test10(self):
-        return parser('o_o :- ((g);(h)).').process()[0] == -1
-
-    def good_test11(self):
-        return parser('fAb1 :- ((g),(h)).').process()[0] == -1
-
-    def bad_test1(self):
-        return parser('f').process()[0] != -1
-
-    def bad_test2(self):
-        return parser('f : g.').process()[0] != -1
-
-    def bad_test3(self):
-        return parser('f :- g, h; t).').process()[0] != -1
-
-    def bad_test4(self):
-        return parser('f :- g, (h t).').process()[0] != -1
-
-    def bad_test5(self):
-        return parser('f :- (g),h).').process()[0] != -1
-
-    def bad_test6(self):
-        return parser(' :- (g);h.').process()[0] != -1
-
-    def bad_test7(self):
-        return parser('f :- ((g),(h)').process()[0] != -1
-
-    def bad_test8(self):
-        return parser('f ((g);(h)).').process()[0] != -1
-
-    def bad_test9(self):
-        return parser('f : - g.').process()[0] != -1
-
-    def bad_test10(self):
-        return parser('f :- g h.').process()[0] != -1
-
-    def bad_test11(self):
-        return parser('1f :- g h.').process()[0] != -1
-
-
 if __name__ == "__main__":
-    Tester().run()
+    test.Tester().run()
     filename = sys.argv[1]
     file = open(filename, 'r')
     inp = file.read()
